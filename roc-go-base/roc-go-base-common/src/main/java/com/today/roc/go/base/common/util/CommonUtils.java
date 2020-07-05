8af885a035a017521cbe3ece45b183fa71e9ad42
git_zcp <cp.zou@caimei365.com>
2020-07-05 10:43:51 +0800

    update pom dependencies

---
+++ roc-go-base/roc-go-base-common/src/main/java/com/today/roc/go/base/common/util/CommonUtils.java
@@ -0,0 +1,18 @@
+package com.today.roc.go.base.common.util;
+
+/**
+ * ^---^---^---^---^---^---^---^
+ * --v---v---v---v---v---v---v--
+ *
+ * @author zou.cp
+ * @version 1.0
+ * @Description
+ * @createTime 2020年07月05日 10:17*
+ * log.info()
+ */
+public class CommonUtils {
+
+    public static void init(){
+        System.out.println("init");
+    }
+}
---
+++ roc-go-common/src/main/java/com/today/roc/go/common/utils/reflect/ReflectUtils.java
@@ -0,0 +1,19 @@
+package com.today.roc.go.common.utils.reflect;
+
+/**
+ * ^---^---^---^---^---^---^---^
+ * --v---v---v---v---v---v---v--
+ *
+ * @author zou.cp
+ * @version 1.0
+ * @Description
+ * @createTime 2020年07月05日 10:17*
+ * log.info()
+ */
+public class ReflectUtils {
+
+    public static void main(String[] args) {
+
+    }
+
+}

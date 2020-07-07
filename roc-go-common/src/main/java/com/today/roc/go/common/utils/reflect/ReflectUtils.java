db23fc5e156d86a93853b9a608fd4913c47382d6
git_zcp <cp.zou@caimei365.com>
2020-07-08 00:40:21 +0800

    reflect learn

--- roc-go-common/src/main/java/com/today/roc/go/common/utils/reflect/ReflectUtils.java
+++ roc-go-common/src/main/java/com/today/roc/go/common/utils/reflect/ReflectUtils.java
@@ -1,5 +1,12 @@
 package com.today.roc.go.common.utils.reflect;
 
+import com.today.roc.go.common.bo.JsonBO;
+import com.today.roc.go.common.utils.json.JsonUtils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * ^---^---^---^---^---^---^---^
  * --v---v---v---v---v---v---v--
@@ -12,7 +19,30 @@ package com.today.roc.go.common.utils.reflect;
  */
 public class ReflectUtils {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        JsonBO jsonBO = JsonUtils.buildObject(JsonBO.class, 10);
+        testForName();
+        Class<? extends JsonBO> aClass = jsonBO.getClass();
+
+    }
+
+
+    public static void testForName() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        Class<?> aClass = Class.forName("com.today.roc.go.common.bo.JsonBO");
+        Object o = aClass.newInstance();
+        Field[] declaredFields = aClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println(declaredField);
+        }
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        for (Method  declaredMethod: declaredMethods) {
+            System.out.println(declaredMethod);
+        }
+        Constructor<?>[] declaredConstructors = aClass.getDeclaredConstructors();
+        for (Constructor<?> declaredConstructor : declaredConstructors) {
+            System.out.println(declaredConstructor);
+        }
+
 
     }
 
